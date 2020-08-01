f0217a8206079a1eba4fd38697ecceb147dc198c
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-08-01 11:31:35 +0100

    add getTodo

--- src/pages/EditTodoPage.jsx
+++ src/pages/EditTodoPage.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import { getTodo } from "../graphql/queries";
 import { PageHeader } from "antd";
-import { Layout, Card, Button } from "antd";
+import { Layout, Card, Button, Spin } from "antd";
 import { Link } from "react-router-dom";
 
 const { Content } = Layout;
 
-const EditTodoPage = () => {
+const EditTodoPage = ({ location }) => {
+  const loadingState = false;
+  const [todo, setTodo] = useState({});
+  const [loadingComplete, setloadingComplete] = useState(loadingState);
+
+  async function fetchTodo() {
+    try {
+      const id = location.pathname.split("/")[2];
+      const todo = await API.graphql(graphqlOperation(getTodo, { id }));
+      setTodo(todo);
+      setloadingComplete({ loadingComplete: true });
+    } catch (err) {
+      console.log("error fetching todo");
+    }
+  }
+
+  useEffect(() => {
+    fetchTodo();
+  }, []);
   return (
     <div>
       <Content style={{ padding: "0 50px" }}>
@@ -16,13 +36,17 @@ const EditTodoPage = () => {
             style={styles.header}
           />
         </div>
-        <Card title={"To-do"} style={{ width: 300 }}>
-          <p>"Description"</p>
-          <Button type="primary">Save</Button>
-          <Button>
-            <Link className="button">Back</Link>
-          </Button>
-        </Card>
+        {loadingComplete ? (
+          <Card title={"To-do"} style={{ width: 300 }}>
+            <p>"Description"</p>
+            <Button type="primary">Save</Button>
+            <Button>
+              <Link className="button">Back</Link>
+            </Button>
+          </Card>
+        ) : (
+          <Spin />
+        )}
       </Content>
     </div>
   );
